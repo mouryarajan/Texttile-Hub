a455eff2b57142557d98a1739fc31cd2dd2a447d
Rajan Mourya <rajanmourya70@gmail.com>
2021-01-26 21:41:06 +0530

    quantity

--- routes/r-products.js
+++ routes/r-products.js
@@ -6,7 +6,7 @@ const productseController = require('../controller/c-products');
 
 //Routes
 router.post('/add-products', productseController.postProducts);
-router.post('/product-list', productseController.getProducts);
+router.get('/product-list', productseController.getProducts);
 router.get('/size', productseController.getSize);
 
 router.post('/post-review', productseController.postReview);
